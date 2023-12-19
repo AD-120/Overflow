f6320854c72ace1d194d22cdffcaaa8db9e3d43d
AD120 <avrahaaam@gmail.com>
2023-12-19 10:46:08 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -71,7 +71,7 @@ function drawTime(index, PersonDate) {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  digitSize = windowWidth/20  ;
+  digitSize = windowWidth/10  ;
 }
 
 function draw() {
